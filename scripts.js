b32e4a7d4e544ec71ac88203f9aeb4a2b5903365
Paul O <info@paulooidesign.com>
2019-09-14 02:40:23 -0400

    add quotes and functionality

---
+++ scripts.js
@@ -0,0 +1,127 @@
+var quoteArray = [
+    "Two roads diverged in a wood and I - I took the one less traveled by, and that has made all the difference.",
+    "In three words I can sum up everything I've learned about life: it goes on.",
+    "The woods are lovely, dark and deep. But I have promises to keep, and miles to go before I sleep.",
+    "Half the world is composed of people who have something to say and can't, and the other half who have nothing to say and keep on saying it.",
+    "Poetry is when an emotion has found its thought and the thought has found words.",
+    "Education is the ability to listen to almost anything without losing your temper or your self-confidence.",
+    "There are two kinds of teachers: the kind that fill you with so much quail shot that you can't move, and the kind that just gives you a little prod behind and you jump to the skies.",
+    "The brain is a wonderful organ; it starts working the moment you get up in the morning and does not stop until you get into the office.",
+    "A mother takes twenty years to make a man of her boy, and another woman makes a fool of him in twenty minutes.",
+    "Forgive me my nonsense, as I also forgive the nonsense of those that think they talk sense.",
+    "My sorrow, when she's here with me, thinks these dark days of autumn rain are beautiful as days can be; she loves the bare, the withered tree; she walks the sodden pasture lane.",
+    "Poetry is about the grief. Politics is about the grievance.",
+    "Don't ever take a fence down until you know why it was put up.",
+    "The strongest and most effective force in guaranteeing the long-term maintenance of power is not violence in all the forms deployed by the dominant to control the dominated, but consent in all the forms in which the dominated acquiesce in their own domination.",
+    "We dance round in a ring and suppose, but the secret sits in the middle and knows.",
+    "I would have written of me on my stone: I had a lover's quarrel with the world.",
+    "A bank is a place where they lend you an umbrella in fair weather and ask for it back when it begins to rain.",
+    "The best way out is always through.",
+    "A diplomat is a man who always remembers a woman's birthday but never remembers her age.",
+    "By working faithfully eight hours a day you may eventually get to be boss and work twelve hours a day.",
+    "A liberal is a man too broadminded to take his own side in a quarrel.",
+    "Some say the world will end in fire, some say in ice.",
+    "I have never started a poem yet whose end I knew. Writing a poem is discovering.",
+    "Happiness makes up in height for what it lacks in length.",
+    "The world is full of willing people; some willing to work, the rest willing to let them.",
+    "A jury consists of twelve persons chosen to decide who has the better lawyer.",
+    "Poetry is a way of taking life by the throat.",
+    "There never was any heart truly great and generous, that was not also tender and compassionate.",
+    "Home is the place where, when you have to go there, they have to take you in.",
+    "The reason why worry kills more people than work is that more people worry than work.",
+    "You don't have to deserve your mother's love. You have to deserve your father's.",
+    "The ear is the only true writer and the only true reader.",
+    "The father is always a Republican toward his son, and his mother's always a Democrat.",
+    "Take care to sell your horse before he dies. The art of life is passing losses on.",
+    "A successful lawsuit is the one worn by a policeman.",
+    "A poem begins in delight and ends in wisdom.",
+    "The best things and best people rise out of their separateness; I'm against a homogenized society because I want the cream to rise.",
+    "I always entertain great hopes.",
+    "The jury consist of twelve persons chosen to decide who has the better lawyer.",
+    "I hold it to be the inalienable right of anybody to go to hell in his own way.",
+    "I had a lovers quarrel with the world.",
+    "To be a poet is a condition, not a profession.",
+    "If society fits you comfortably enough, you call it freedom.",
+    "Thinking isn't agreeing or disagreeing. That's voting.",
+    "Being the boss anywhere is lonely. Being a female boss in a world of mostly men is especially so.",
+    "Let him that is without stone among you cast the first thing he can lay his hands on.",
+    "We love things we love what they are.",
+    "And were an epitaph to be my story I'd have a short one ready for my own. I would have written of me on my stone: I had a lover's quarrel with the world.",
+    "A person will sometimes devote all his life to the development of one part of his body - the wishbone.",
+    "The middle of the road is where the white line is - and that's the worst place to drive.",
+    "Love is an irresistible desire to be irresistibly desired.",
+    "Always fall in with what you're asked to accept. Take what is given, and make it over your way. My aim in life has always been to hold my own with whatever's going. Not against: with.",
+    "Freedom lies in being bold.",
+    "It's a funny thing that when a man hasn't anything on earth to worry about, he goes off and gets married.",
+    "I'm not confused. I'm just well mixed.",
+    "The figure a poem makes. It begins in delight and ends in wisdom... in a clarification of life - not necessarily a great clarification, such as sects and cults are founded on, but in a momentary stay against confusion.",
+    "Forgive, O Lord, my little jokes on Thee, and I'll forgive Thy great big joke on me.",
+    "What we live by we die by.",
+    "I never dared to be radical when young for fear it would make me conservative when old.",
+    "Most of the change we think we see in life is due to truths being in and out of favor.",
+    "The artist in me cries out for design.",
+    "Education doesn't change life much. It just lifts trouble to a higher plane of regard.",
+    "A civilized society is one which tolerates eccentricity to the point of doubtful sanity.",
+    "You can be a rank insider as well as a rank outsider.",
+    "The only certain freedom's in departure.",
+    "No tears in the writer, no tears in the reader. No surprise in the writer, no surprise in the reader.",
+    "If you don't know how great this country is, I know someone who does; Russia.",
+    "Nobody was ever meant, To remember or invent, What he did with every cent.",
+    "The greatest thing in family life is to take a hint when a hint is intended-and not to take a hint when a hint isn't intended.",
+    "No memory of having starred atones for later disregard, or keeps the end from being hard.",
+    "If one by one we counted people out For the least sin, it wouldn't take us long To get so we had no one left to live with. For to be social is to be forgiving.",
+    "I often say of George Washington that he was one of the few in the whole history of the world who was not carried away by power.",
+    "You have freedom when you're easy in your harness.",
+    "The worst disease which can afflict executives in their work is not, as popularly supposed, alcoholism; it's egotism.",
+    "The only way round is through.",
+    "Style is that which indicates how the writer takes himself and what he is saying. It is the mind skating circles around itself as it moves forward.",
+    "I'd just as soon play tennis with the net down.",
+    "Hell is a half-filled auditorium.",
+    "College is a refuge from hasty judgment.",
+    "You can't get too much winter in the winter.",
+    "You can be a little ungrammatical if you come from the right part of the country.",
+    "Talking is a hydrant in the yard and writing is a faucet upstairs in the house. Opening the first takes the pressure off the second.",
+    "Humor is the most engaging cowardice.",
+    "Two such as you with such a master speed, cannot be parted nor be swept away, from one another once you are agreed, that life is only life forevermore, together wing to wing and oar to oar.",
+    "Poets are like baseball pitchers. Both have their moments. The intervals are the tough things.",
+    "Education is hanging around until you've caught on.",
+    "A poem begins as a lump in the throat, a sense of wrong, a homesickness, a lovesickness.",
+    "Pressed into service means pressed out of shape.",
+    "Poetry is what gets lost in translation.",
+    "They would not find me changed from him they knew - only more sure of all I thought was true.",
+    "Modern poets talk against business, poor things, but all of us write for money. Beginners are subjected to trial by market.",
+    "The chief reason for going to school is to get the impression fixed for life that there is a book side for everything.",
+    "I alone of English writers have consciously set myself to make music out of what I may call the sound of sense.",
+    "To be social is to be forgiving.",
+    "Nothing can make injustice just but mercy.",
+    "I am a writer of books in retrospect. I talk in order to understand; I teach in order to learn.",
+    "A poet never takes notes. You never take notes in a love affair.",
+    "One aged man - one man - can't fill a house.",
+    "Writing free verse is like playing tennis with the net down.",
+    "Space ails us moderns: we are sick with space.",
+    "I go to school the youth to learn the future."
+  ];
+var generateBtn = document.querySelector('button.generate');
+var quoteDisplay = document.querySelector('blockquote .quote');
+
+generateBtn.addEventListener('click', function(){
+    let quote = generateQuote(quoteArray);
+    quoteDisplay.textContent = quote; // ASSIGN THE NEW QUOTE
+}, false);
+
+document.addEventListener('DOMContentLoaded', function(){
+    generateBtn.click();
+})
+
+function generateQuote(quoteArray) {
+    let newQuote = "";
+    let existingQuote = document.querySelector('blockquote .quote').textContent;
+
+    /* PICK NEW QUOTE, CANNOT BE THE SAME QUOTE AGAIN */
+    do {
+        let randomIndex = Math.floor(Math.random() * +quoteArray.length);
+        newQuote = quoteArray[randomIndex];
+    } while (newQuote == existingQuote);
+
+    return newQuote;
+}
